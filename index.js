02a420952bc11b471505c6d85edc293322ee3d45
unknown <angelobeckan794@gmail.com>
2022-07-17 00:54:02 +0100

    commit1

---
+++ index.js
@@ -0,0 +1,16 @@
+var express= require("express")
+var body= require("body-parser")
+var ejs = require("ejs")
+const path= require("path")
+const { request, response } = require("express")
+var app = express()
+
+app.use(body.urlencoded({extended:true}))
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+
+app.get("/", (request, response)=>{
+    day= new Date().toLocaleDateString()
+    response.render("index"), {day:day}})
+app.listen(process.env.PORT || 3000, ()=>{ console.log("ready to launch!")})
